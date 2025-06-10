229281939b4f8e080289241648cf397e3d1c3288
jasony9982 <jason.yuan12@gmail.com>
2025-06-11 01:42:35 +0800

    Create TestResponse.js

---
+++ TestResponse.js
@@ -0,0 +1,12 @@
+$done({
+  body: JSON.stringify({
+    status: 1,
+    message: "成功",
+    data: {
+      easebean: {
+        balance: 201,
+        qrCode: "easebean:s+UysvZNc3IaF0DsMS0UZnHLmoAC3/leIBOYqxZRb7o="
+      }
+    }
+  })
+});
