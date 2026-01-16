dc907dd73a08f26f8844133bcb059183094667c5
jasony9982 <jason.yuan12@gmail.com>
2026-01-17 00:21:01 +0800

    Update douyin.js

--- douyin.js
+++ douyin.js
@@ -1,8 +1,3 @@
 DOMAIN-KEYWORD, tiktokcdn, 🌈飞跃彩虹
 DOMAIN-KEYWORD, tiktokv, 🌈飞跃彩虹
 
-DOMAIN-KEYWORD, zga.globalconnetct.com, 🌈飞跃彩虹
-DOMAIN-KEYWORD, sgameglobal, 🌈飞跃彩虹
-host-suffix, wetest.net, 抖音
-host-suffix, vasdgame.com, 抖音
-host-suffix, intlgame.com, 抖音
